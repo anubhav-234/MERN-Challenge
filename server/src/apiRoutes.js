640c6136e96a8f12a88041ef149ddf1643b01878
Anubhav <121926452+anubhav-235@users.noreply.github.com>
2024-04-15 15:28:17 +0530

    Bug Fixes

--- server/src/apiRoutes.js
+++ server/src/apiRoutes.js
@@ -2,11 +2,9 @@
 
 const express = require('express');
 const test1Router = require('./users/routes.js');
-const test2Router = require('./test2/routes.js');
 
 const combinedRouter = express.Router();
 
 combinedRouter.use('/users', test1Router);
-combinedRouter.use('/test2', test2Router);
 
 module.exports = combinedRouter;
